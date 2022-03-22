e4625f30b5ec062b1659392c6be50f933967e5ff
Roxana <rox050996@gmail.com>
2022-03-22 11:51:07 -0600

    add assets

---
+++ js/brainforth.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        items:1,
+    margin:10,
+    autoHeight:true,
+    autoHeightClass: 'owl-height'
+    });
+  });
+  
